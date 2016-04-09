3d7142f249d711984a873a5dc7c21a3b76b39a0e
unknown <deftsoft9211@gmail.com>
2016-04-09 19:06:09 +0530

    add file for convert php array to xml files

---
+++ arraytoxml.php
@@ -0,0 +1,15 @@
+<?php 
+
+
+how to make array to xml in php
+<?php
+$test_array = array (
+  'bla' => 'blub',
+  'foo' => 'bar',
+  'another_array' => array (
+    'stack' => 'overflow',
+  ),
+);
+$xml = new SimpleXMLElement('<root/>');
+array_walk_recursive($test_array, array ($xml, 'addChild'));
+print $xml->asXML();
